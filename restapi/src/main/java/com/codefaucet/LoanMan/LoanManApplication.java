d49c8f95c6ad553ea3f3d9398e374d6405e310a6
joshua <zabalajoshua@live.com.ph>
2020-02-24 23:20:05 +0800

    Initial commit.

---
+++ restapi/src/main/java/com/codefaucet/LoanMan/LoanManApplication.java
@@ -0,0 +1,13 @@
+package com.codefaucet.LoanMan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanManApplication.class, args);
+	}
+
+}
---
+++ restapi/src/test/java/com/codefaucet/LoanMan/LoanManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefaucet.LoanMan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
